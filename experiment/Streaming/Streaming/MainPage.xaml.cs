f586d22cebebf949cb11fd9361d078a38d9fa176
Jordan Knight <jakkaj@gmail.com>
2016-09-03 09:20:46 +1000

    Added streaming logs example

---
+++ experiment/Streaming/Streaming/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Streaming
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+
+                httpClient.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
+                var requestUri = "https://functionplayground.scm.azurewebsites.net/logstream";
+
+                var user = "";
+                var pass = "";
+               
+                var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes($"{user}:{pass}")));
+
+                var response = await httpClient.SendAsync(
+                    request, HttpCompletionOption.ResponseHeadersRead);
+                var stream = await response.Content.ReadAsStreamAsync();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Task.Run(async () =>
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+
+                            //We are ready to read the stream
+                            var currentLine = reader.ReadLine();
+                            Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                            {
+                                TxtOutput.Text += $"\r\n{currentLine}";
+                            });
+
+                            Debug.WriteLine(currentLine);
+                            await Task.Yield();
+                        }
+                    }
+                });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+
+            }
+        }
+    }
+}
