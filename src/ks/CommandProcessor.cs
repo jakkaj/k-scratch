518de4872528b62f82f2df7ab32707c63b749c98
Jordan Knight <jakkaj@outlook.com>
2016-10-06 13:56:48 +1100

    Added logger - but tooling is pretty broken, cannot build in VS.

--- src/ks/CommandProcessor.cs
+++ src/ks/CommandProcessor.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.CommandLine;
 using System.Linq;
 using System.Threading.Tasks;
-
+using KScratch.Contract.Services;
 namespace ks
 {
     public class CommandProcessor
     {
+        private readonly ILocalLogService _logService;
+
+        public CommandProcessor(ILocalLogService logService)
+        {
+            _logService = logService;
+        }
         public int Process(string[] args)
         {
-            var addressee = "world";
-
             if (args == null || args.Length == 0)
             {
                 args = new[] {"-h"};
             }
+
+            _logService.Log("Welcome to k-scratch. Someversion number.");
 
             var command = string.Empty;
             var prune = false;
@@ -26,12 +32,12 @@ namespace ks
             {
                 //syntax.DefineOption("n|name", ref addressee, "The addressee to greet");
 
-                syntax.DefineCommand("pull", ref command, "Pull from another repo");
-                syntax.DefineOption("p|prune", ref prune, "Prune branches");
+                syntax.DefineCommand("log", ref command, "Stream the log to the command window");
+                //syntax.DefineOption("p|prune", ref prune, "Prune branches");
 
-                syntax.DefineCommand("commit", ref command, "Committing changes");
-                syntax.DefineOption("m|message", ref message, "The message to use");
-                syntax.DefineOption("amend", ref amend, "Amend existing commit");
+                //syntax.DefineCommand("commit", ref command, "Committing changes");
+                //syntax.DefineOption("m|message", ref message, "The message to use");
+                //syntax.DefineOption("amend", ref amend, "Amend existing commit");
             });
 
 
