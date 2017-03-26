3d441b88316a6038e209c0c65ce2a4593263f98b
Jordan Knight <jorkni@microsoft.com>
2017-03-26 20:36:45 +1100

    Test runs working by number from command prompt

--- src/ks/Program.cs
+++ src/ks/Program.cs
@@ -7,7 +7,7 @@ using Autofac;
 using ks.Glue;
 using ks.model.Contract.Services;
 using ks.model.Glue;
-
+using ks.model.Services;
 
 namespace ks
 {
@@ -30,8 +30,33 @@ namespace ks
 
             if (!initResult.Item2) return initResult.Item1;
 
+            var testService = _glue.Container.Resolve<ITestService>();
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                if(key.Key == ConsoleKey.Escape)
+                {
+                    return initResult.Item1;
+                }
+
+                var i = (int)key.Key;
+
+                //49 is 1. We want to start from 0 @ 49
+                if(i >= 49 && i <= 57)
+                {
+                    var numb = i - 48;
+
+                    if (numb >= 0)
+                    {
+                        testService.RunTest(numb);
+                    }
+                }                
+
+                System.Threading.Thread.Sleep(250);
+            }
             
-            Console.ReadLine();
 
             return initResult.Item1;
         }
